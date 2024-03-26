c9061358e559cff925f1f67f06938735109e9f69
aroua <aroua.bendaya@etudiant-fst.utm.tn>
2024-03-26 19:27:28 +0100

    implement model entities

--- src/main/java/org/arouabendaya/theresidencehotel/model/BookedRoom.java
+++ src/main/java/org/arouabendaya/theresidencehotel/model/BookedRoom.java
@@ -1,4 +1,70 @@
 package org.arouabendaya.theresidencehotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id //set the primary key of the table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //how the primary key is going to get generated
+    private Long BookingId ;
+
+    @Column (name = "check_In")
+    private LocalDate checkInDate ;
+
+
+    @Column (name = "check_Out")
+    private LocalDate getCheckInDateDate ;
+
+    @Column (name = "guest_FullName")
+    private String guestFullName ;
+
+    @Column (name = "guest_Email")
+    private String guestEmail ;
+
+    @Column (name = "adults")
+    private int NumOfAdults ;
+
+    @Column (name = "children")
+    private int NumOfChildren ;
+
+    @Column (name = "total_guest")
+    private int totalNumOfGuest ;
+
+    @Setter
+    @Column (name = "confirmtion_Code")
+    private String bookingConformationCode ;
+
+    //A many-to-one mapping : many instances of guest entity are mapped to one instance of room entity
+    @ManyToOne(fetch = FetchType.LAZY)
+
+    @JoinColumn (name = "room_id")
+    private Room room ;
+
+    public void calculateTotalNumberOfGuest (){
+        this.totalNumOfGuest =this.NumOfAdults +this.NumOfChildren ;
+
+    }
+
+    public void setNumOfAdults (int numOfAdults){
+        NumOfAdults =numOfAdults ;
+        calculateTotalNumberOfGuest ();
+    }
+
+    public void  setNumOfChildren (int numOfChildren){
+        NumOfChildren = numOfChildren ;
+        calculateTotalNumberOfGuest ();
+    }
+
+
 }
--- src/main/java/org/arouabendaya/theresidencehotel/model/Room.java
+++ src/main/java/org/arouabendaya/theresidencehotel/model/Room.java
@@ -1,5 +1,47 @@
 package org.arouabendaya.theresidencehotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id //set the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id ;
+    private  String roomType ;
+    private BigDecimal roomPrice ;
+    private boolean isBooked = false ;
+
+    @Lob //large object
+    private Blob photo ;
+    @OneToMany (mappedBy="room" ,fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings ;
+
+    public Room(){
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking (BookedRoom booking){
+        if (booking == null){
+            bookings = new ArrayList<>() ;
+        }
+        bookings.add(booking);
+        assert booking != null;
+        booking.setRoom(this);
+        isBooked =true ;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConformationCode(bookingCode);
+
+    }
 
 }
